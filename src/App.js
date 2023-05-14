ca4358046bbdd5eeac89090a3cd83388aeba8673
adhayan002 <96379455+adhayan002@users.noreply.github.com>
2023-05-14 22:16:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [searchValue,setSearchValue]=useState('')
 
   const getMovieRequest=async(searchValue)=>{
-    const url=`http://www.omdbapi.com/?s=${searchValue}&apikey=d38e7913`
+    const url=`https://www.omdbapi.com/?s=${searchValue}&apikey=d38e7913`
     
     const response=await fetch(url)
     const responseJson=await response.json()
